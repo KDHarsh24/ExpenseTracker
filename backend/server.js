1f700e3375056c5fdfa96656e08b16019c7f214e
kdharsh24 <harshkumardas24@gmail.com>
2025-03-19 22:05:12 +0530

    free it

--- backend/server.js
+++ backend/server.js
@@ -11,25 +11,8 @@ const expenseRoutes = require("./routes/expenseRoutes");
 connectDB();
 const app = express();
 
-app.use(cors({
-    origin: '*', // Allows requests from any origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Allow cookies/auth headers
-  }));
+app.use(cors());
   
-  // OR, using manual headers:
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-  
-    if (req.method === "OPTIONS") {
-      return res.status(200).end(); // Respond to preflight request
-    }
-    next();
-  });
 app.use(express.json());
 app.use(cookieParser());
 
