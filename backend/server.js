205d367e73320c999042ca4ff1bfa744393b274b
kdharsh24 <harshkumardas24@gmail.com>
2025-03-19 21:58:48 +0530

    allowing

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const expenseRoutes = require("./routes/expenseRoutes");
 connectDB();
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://expensetracker-wine-rho.vercel.app" }));
 app.use(express.json());
 app.use(cookieParser());
 
